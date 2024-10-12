61278e03e6403fdaaa857c8233c7e48dbaeda171
leoo84 <leogarnaud84@gmail.com>
2024-10-12 16:37:54 +0200

    fix README and start-py.sh again

--- start-py.sh
+++ start-py.sh
@@ -2,7 +2,7 @@
 python3 -m venv venv
 source venv/bin/activate
 echo "Installing dependencies..."
-pip install -q pycryptodome beautifulsoup4 requests autoslot tabulate pronotepy openpyxl pandas fpdf
+pip install -q pycryptodome beautifulsoup4 requests autoslot tabulate pronotepy openpyxl pandas fpdf xlsxwriter
 
 # Prompt the user for the Python file to execute and run it
 read -p "Enter the Python file to execute (e.g., all-scripts/get_meals.py): " python_file

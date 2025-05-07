b8369b6e14326405f21b22c2902fd02ec10fa10a
leoo84 <leogarnaud84@gmail.com>
2025-05-07 21:51:47 +0200

    add a confirmation message after the dependencies installation complete

--- setup-windows.py
+++ setup-windows.py
@@ -4,7 +4,7 @@ import subprocess
 # Install necessary libraries
 print("Installing dependencies...")
 pip.main(['install', 'pycryptodome', 'beautifulsoup4', 'requests', 'autoslot', 'tabulate', 'pronotepy', 'openpyxl', 'pandas', 'fpdf', 'xlsxwriter'])
-
+print("Dependencies installed.")
 # Prompt the user for the Python file to execute and run it
 python_file = input("Enter the Python file to execute (e.g., all-scripts/get_meals.py): ")
 
